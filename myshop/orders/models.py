e1265a474d55ecfdb48108ea00fc694261985989
Viktor <76591811+BoykoVik@users.noreply.github.com>
2021-11-21 18:36:41 +0300

    6

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -15,6 +15,8 @@ class Order(models.Model):
 
     class Meta:
         ordering = ('-created',)
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
 
     def __str__(self):
         return 'Order {}'.format(self.id)
